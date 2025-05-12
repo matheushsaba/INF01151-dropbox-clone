dc0df5fb61ae8e474de651815cb480b96c3d455a
Maiara Trevisol <maiara.trevisol@sap.com>
2025-05-11 21:50:52 -0300

    packet implementation

--- common/packet.cpp
+++ common/packet.cpp
@@ -1,25 +1,57 @@
-#include <iostream>
-#include <string>
-#include <cstring>
 #include "packet.h"
+#include <cstring>
+#include <sys/socket.h>
 
-int main () {
-	packet p;
-	
-	//struct test:
-	/*
-    	p.type = 1;
-    	p.seqn = 42;
-    	p.total_size = 1024;
-    	p.lenght = 256;
-    	p.payload = "This is test data";
+int serialize_packet(const Packet& pkt, char* buffer, size_t buffer_size) {
+    if (buffer_size < sizeof(pkt.type) + sizeof(pkt.seqn) + sizeof(pkt.total_size) + sizeof(pkt.length) + pkt.length) {
+        return -1;
+    }
 
-    	std::cout << "Packet Info:\n";
-    	std::cout << "Type: " << p.type << "\n";
-    	std::cout << "Seqn: " << p.seqn << "\n";
-    	std::cout << "Total Size: " << p.total_size << "\n";
-    	std::cout << "Length: " << p.lenght << "\n";
-    	std::cout << "Payload: " << p.payload << "\n";*/
-    	
-	return 0;
+    size_t offset = 0;
+    std::memcpy(buffer + offset, &pkt.type, sizeof(pkt.type)); offset += sizeof(pkt.type);
+    std::memcpy(buffer + offset, &pkt.seqn, sizeof(pkt.seqn)); offset += sizeof(pkt.seqn);
+    std::memcpy(buffer + offset, &pkt.total_size, sizeof(pkt.total_size)); offset += sizeof(pkt.total_size);
+    std::memcpy(buffer + offset, &pkt.length, sizeof(pkt.length)); offset += sizeof(pkt.length);
+    std::memcpy(buffer + offset, pkt.payload, pkt.length);
+
+    return offset + pkt.length;
+}
+
+int deserialize_packet(const char* buffer, size_t buffer_size, Packet& pkt) {
+    size_t offset = 0;
+    if (buffer_size < sizeof(pkt.type) + sizeof(pkt.seqn) + sizeof(pkt.total_size) + sizeof(pkt.length)) return -1;
+
+    std::memcpy(&pkt.type, buffer + offset, sizeof(pkt.type)); offset += sizeof(pkt.type);
+    std::memcpy(&pkt.seqn, buffer + offset, sizeof(pkt.seqn)); offset += sizeof(pkt.seqn);
+    std::memcpy(&pkt.total_size, buffer + offset, sizeof(pkt.total_size)); offset += sizeof(pkt.total_size);
+    std::memcpy(&pkt.length, buffer + offset, sizeof(pkt.length)); offset += sizeof(pkt.length);
+
+    if (pkt.length > MAX_PAYLOAD_SIZE || buffer_size < offset + pkt.length) return -1;
+
+    std::memcpy(pkt.payload, buffer + offset, pkt.length);
+    return offset + pkt.length;
+}
+
+bool send_packet(int sockfd, const Packet& pkt) {
+    char buffer[1500];
+    int size = serialize_packet(pkt, buffer, sizeof(buffer));
+    if (size < 0) return false;
+    return send(sockfd, buffer, size, 0) == size;
+}
+
+bool recv_packet(int sockfd, Packet& pkt) {
+    char header[10];
+    int header_size = sizeof(pkt.type) + sizeof(pkt.seqn) + sizeof(pkt.total_size) + sizeof(pkt.length);
+    if (recv(sockfd, header, header_size, MSG_WAITALL) != header_size) return false;
+
+    uint16_t length;
+    std::memcpy(&length, header + header_size - sizeof(length), sizeof(length));
+
+    if (length > MAX_PAYLOAD_SIZE) return false;
+
+    char buffer[1500];
+    std::memcpy(buffer, header, header_size);
+    if (recv(sockfd, buffer + header_size, length, MSG_WAITALL) != length) return false;
+
+    return deserialize_packet(buffer, header_size + length, pkt) > 0;
 }
--- common/packet.h
+++ common/packet.h
@@ -1,10 +1,34 @@
 #pragma once
 #include <cstdint>
 #include <string>
-typedef struct packet {
-	uint16_t type; //data or cmd
-	uint16_t seqn;
-	uint32_t total_size; //amount of fragments
-	uint16_t lenght;
-	std::string payload; //package data
-} packet;
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <cstdint>
+
+#define MAX_PAYLOAD_SIZE 1024
+
+enum PacketType {
+    PACKET_TYPE_CMD = 1,
+    PACKET_TYPE_DATA = 2,
+    PACKET_TYPE_ACK = 3
+};
+
+struct Packet {
+    uint16_t type;
+    uint16_t seqn;
+    uint32_t total_size;
+    uint16_t length;
+    char payload[MAX_PAYLOAD_SIZE];
+};
+
+// Serialização / Desserialização
+int serialize_packet(const Packet& pkt, char* buffer, size_t buffer_size);
+int deserialize_packet(const char* buffer, size_t buffer_size, Packet& pkt);
+
+// Envio / Recebimento via socket
+bool send_packet(int sockfd, const Packet& pkt);
+bool recv_packet(int sockfd, Packet& pkt);
+
+#endif
+
